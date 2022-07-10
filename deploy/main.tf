a02d206bbf4afc9dbb239a0327200e86ea173ab1
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-10 19:18:17 +0900

    terraform format

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,25 +1,25 @@
 terraform {
-    backend "s3" {
-        bucket = "recipe-app-api-devops-tfstate"
-        key = "recipe-app.tfstate"
-        region = "us-east-1"
-        encrypt = true
-        dynamodb_table = "recipe-app-api-devops-tf-state-lock"
-    }
+  backend "s3" {
+    bucket         = "recipe-app-api-devops-tfstate"
+    key            = "recipe-app.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "recipe-app-api-devops-tf-state-lock"
+  }
 
 }
 
 provider "aws" {
-    region = "us-east-1"
-    version = "~> 2.54.0"
+  region  = "us-east-1"
+  version = "~> 2.54.0"
 }
 
 locals {
-    prefix = "${var.prefix}-${terraform.workspace}"
-    common_tags = {
-        Environment = terraform.workspace
-        Project = var.project
-        Owner = var.contact
-        ManageBy = "Terraform"
-    }
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    Owner       = var.contact
+    ManageBy    = "Terraform"
+  }
 }
