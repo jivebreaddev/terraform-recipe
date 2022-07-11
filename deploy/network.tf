6dcae9302afdf99fa61cb6e416e83de92a6dd0f0
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-11 17:06:27 +0900

    fmt terraform

--- deploy/network.tf
+++ deploy/network.tf
@@ -139,6 +139,17 @@ resource "aws_subnet" "private_a" {
 
 }
 
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.1.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
 resource "aws_route_table" "private_a" {
   vpc_id = aws_vpc.main.id
 
@@ -155,7 +166,7 @@ resource "aws_route_table_association" "private_a" {
 
 resource "aws_route" "private_a_internet_out" {
   route_table_id         = aws_route_table.private_a.id
-  aws_nat_gateway        = aws_nat_gateway.public_a.id
+  nat_gateway_id         = aws_nat_gateway.public_a.id
   destination_cidr_block = "0.0.0.0/0"
 }
 
@@ -171,15 +182,12 @@ resource "aws_subnet" "private_b" {
 }
 
 resource "aws_route_table" "private_b" {
-  cidr_block        = "10.1.11.0/24"
-  vpc_id            = aws_vpc.main.id
-  availability_zone = "${data.aws_region.current.name}b"
+  vpc_id = aws_vpc.main.id
 
   tags = merge(
     local.common_tags,
     map("Name", "${local.prefix}-private-b")
   )
-
 }
 
 resource "aws_route_table_association" "private_b" {
@@ -187,11 +195,8 @@ resource "aws_route_table_association" "private_b" {
   route_table_id = aws_route_table.private_b.id
 }
 
-
 resource "aws_route" "private_b_internet_out" {
   route_table_id         = aws_route_table.private_b.id
-  nat_gateway_id         = aws_nate_gateway.public_b.id
+  nat_gateway_id         = aws_nat_gateway.public_b.id
   destination_cidr_block = "0.0.0.0/0"
 }
-
-
