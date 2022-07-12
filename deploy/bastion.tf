91db9b57fbf8f56cd491befa36ebfbf90306b5cf
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-12 16:22:04 +0900

    Added bastion user data script

--- deploy/bastion.tf
+++ deploy/bastion.tf
@@ -7,6 +7,21 @@ data "aws_ami" "amazon_linux" {
   owners = ["amazon"]
 }
 
+resource "aws_iam_role" "bastion" {
+  name               = "{${local.prefix}-bastion"
+  assume_role_policy = file("./templates/bastion/instsance_profile-policy.json")
+  tags               = local.common_tags
+
+}
+
+resource "aws_iam_role_policy_attachment" "bastion_attach_policy" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${local.prefix}-bastion-instance-profile"
+  role = aws_iam_role.bastion.name
+}
 
 resource "aws_instance" "bastion" {
   ami           = data.aws_ami.amazon_linux.id
