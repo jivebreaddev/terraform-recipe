5bd71c6a7239c709b35f97f1e430c6c5a1539610
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-13 14:51:33 +0900

    Configured S3 image backend

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,9 +30,11 @@ ALLOWED_HOSTS.extend(
     filter(
         None,
         os.environ.get('ALLOWED_HOSTS','').split(',')
+
     )
 )
-
+if os.environ.get('AWS_EXECUTION_ENV'):
+    ALLOWED_HOSTS.append(gethostbyname(gethostname()))
 
 # Application definition
 
@@ -138,3 +140,15 @@ MEDIA_ROOT = '/vol/web/media'
 STATIC_ROOT = '/vol/web/static'
 
 AUTH_USER_MODEL = 'core.User'
+
+S3_STORAGE_BACKEND = bool(int(os.environ.get('S3_STORAGE_BACKEND', 1)))
+if S3_STORAGE_BACKEND is True:
+    DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_STOARGE_BUCKET_NAME = os.environ.get("S3_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = os.environ.get("S3_STORAGE_BUCKET_REGION", 'us-east-1')
+AWS_QUERYSTRING_AUTH = False
+
+
